d837f63e05886564dd233f6c48beccadaa6f8f0f
GhasseneBenSlimene <123106762+GhasseneBenSlimene@users.noreply.github.com>
2023-03-23 12:08:40 +0100

    Firebase Token + Message working

--- app/src/main/java/com/example/chatapp/firebase/MessagingService.java
+++ app/src/main/java/com/example/chatapp/firebase/MessagingService.java
@@ -1,7 +1,5 @@
 package com.example.chatapp.firebase;
 
-import android.util.Log;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MessagingService extends FirebaseMessagingService {
@@ -9,13 +7,11 @@ public class MessagingService extends FirebaseMessagingService {
     @Override
     public void onNewToken(String token) {
         super.onNewToken(token);
-        Log.d("FCM", "Token: " + token);
     }
 
     @Override
     public void onMessageReceived(com.google.firebase.messaging.RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        Log.d("FCM", "Message: " + remoteMessage.getNotification().getBody());
     }
 
 }
