8483c93c7fac071cfb6212697c8edb8d0e01fbb4
GhasseneBenSlimene <123106762+GhasseneBenSlimene@users.noreply.github.com>
2023-03-27 00:01:37 +0100

    video 4 6:50

--- app/src/main/java/com/example/chatapp/activities/MainActivity.java
+++ app/src/main/java/com/example/chatapp/activities/MainActivity.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivityMainBinding;
+import com.example.chatapp.utilities.PreferenceManager;
+import com.example.chatapp.utilities.Constants;
+
+import android.util.Base64;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
     }
+
+    private void loadUserDetails() {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+
+    }
+
 }
