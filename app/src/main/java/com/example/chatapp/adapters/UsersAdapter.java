b440638c0be29d00ca402993db15a81d8939356e
GhasseneBenSlimene <123106762+GhasseneBenSlimene@users.noreply.github.com>
2023-03-28 16:47:01 +0100

    video 6 16:10

--- app/src/main/java/com/example/chatapp/adapters/UsersAdapter.java
+++ app/src/main/java/com/example/chatapp/adapters/UsersAdapter.java
@@ -3,7 +3,10 @@ package com.example.chatapp.adapters;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chatapp.databinding.ItemContainerUserBinding;
@@ -11,12 +14,32 @@ import com.example.chatapp.models.User;
 
 import java.util.List;
 
-public class UsersAdapter {
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UserViewHolder> {
 
     private final List<User> users;
 
     public UsersAdapter(List<User> users) {
         this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemContainerUserBinding itemContainerUserBinding = ItemContainerUserBinding.inflate(
+                        LayoutInflater.from(parent.getContext()),
+                        parent,
+                        false);
+        return new UserViewHolder(itemContainerUserBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        holder.setUserData(users.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
     }
 
     class UserViewHolder extends RecyclerView.ViewHolder {
@@ -27,7 +50,7 @@ public class UsersAdapter {
             binding = itemContainerUserBinding;
         }
 
-        void setUser(User user) {
+        void setUserData(User user) {
             binding.textName.setText(user.name);
             binding.textEmail.setText(user.email);
             binding.imageProfile.setImageBitmap(getUserImage(user.image));
