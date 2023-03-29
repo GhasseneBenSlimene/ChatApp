4f0698e392c2e4162a143879f67be85ed0d58fab
GhasseneBenSlimene <123106762+GhasseneBenSlimene@users.noreply.github.com>
2023-03-29 16:36:26 +0100

    video 7 done

---
+++ app/src/main/java/com/ghassene/chatapp/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.ghassene.chatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ghassene.chatapp.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
